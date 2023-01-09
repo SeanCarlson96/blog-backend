804386a7a25e1e2ea430adcf972cb4c4cb5774c6
SeanCarlson96 <carlsonseanr@gmail.com>
2023-01-09 10:42:10 -0600

    Initializing Project

---
+++ blog-app-be/src/test/java/net/yorksolutions/blogappbe/BlogAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.blogappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
